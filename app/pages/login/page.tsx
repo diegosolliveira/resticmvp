b658c8eed269f92df0363437b1188535e91fdb90
Diego Oliveira <diegolvr14@gmail.com>
2025-03-24 14:21:31 -0300

    fix: corrige tela de login

--- app/pages/login/page.tsx
+++ app/pages/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import ButtonLogin from "../../components/ButtonLogar";
 import InputCadastro from "../../components/InputCadastro";
 import "./styles.css";
@@ -8,38 +9,15 @@ import "./styles.css";
 export default function LoginView() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [errorEmail, setErrorEmail] = useState("");
-    const [errorPassword, setErrorPassword] = useState("");
-
-    const validateForm = () => {
-        let isValid = true;
-        setErrorEmail("");
-        setErrorPassword("");
-
-        if (!email.includes("@")) {
-            setErrorEmail("Digite um email válido.");
-            isValid = false;
-        }
-        if (password.length < 6) {
-            setErrorPassword("A senha deve ter pelo menos 6 caracteres.");
-            isValid = false;
-        }
-
-        return isValid;
-    };
+    const router = useRouter();
 
     const handleLogin = () => {
-        validateForm();
-        console.log("Botão clicado!"); {
-            console.log("Enviando dados...");
-        }
+        router.push("/pages/cadastro");
     };
 
     return (
         <div className="container-login">
-            <div className="container-left">
-
-            </div>
+            <div className="container-left"></div>
 
             <div className="container-right">
                 <div className="form-container">
@@ -55,8 +33,6 @@ export default function LoginView() {
                         onChange={(e) => setEmail(e.target.value)}
                     />
 
-                    {errorEmail && <span className="error">{errorEmail}</span>}
-
                     <InputCadastro
                         label="Senha"
                         name="senha"
@@ -66,10 +42,14 @@ export default function LoginView() {
                         onChange={(e) => setPassword(e.target.value)}
                     />
 
-                    {errorPassword && <span className="error">{errorPassword}</span>}
-
                     <div className="form-container2">
-                        <InputCadastro label="Mantenha-me conectado" name="Mantenha-me conectado" type="checkbox" classInput="input-checkbox" value=""></InputCadastro>
+                        <InputCadastro
+                            label="Mantenha-me conectado"
+                            name="Mantenha-me conectado"
+                            type="checkbox"
+                            classInput="input-checkbox"
+                            value=""
+                        />
                         <a href="">Esqueceu a senha?</a>
                     </div>
 
@@ -86,4 +66,4 @@ export default function LoginView() {
             </div>
         </div>
     );
-}
+};
